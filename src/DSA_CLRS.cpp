28fec0efb8da2ec3b1d12127c6ee52e57ada1cd6
Ken Harvey <kennethken73@gmail.com>
2022-06-21 02:14:24 -0700

    Added Algorithms.org/pdf notes

--- src/DSA_CLRS.cpp
+++ src/DSA_CLRS.cpp
@@ -1,5 +1,11 @@
+/*
+  Main program to run Algorithms on Data Structures.
+  Here, we'll ask the user for their choice of Data Structure,
+  populate it, and run an algorithm of the user's choice on it.
+ */
 #include <iostream>
 
 int main() {
-  std::cout << "Hello, Project:  DSA_CLRS!\n";
+  std::cout << "Welcome to the DSA-runner\n"
+            << "What Data Structure are we setting up?";
 }
